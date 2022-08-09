1a53571829de2485f7d97a2323c94316c97a9e19
Oleh Vdovychenko <ovdovychenko@corelogic.com>
2022-08-09 19:07:06 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("hello");
+
+        int i = 1;
+        System.out.println(++i);
     }
 }
