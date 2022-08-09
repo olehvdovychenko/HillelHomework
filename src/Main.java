7ffa22ea52c9a6df348dba50b6dfdabda4fd9ec5
Oleh Vdovychenko <ovdovychenko@corelogic.com>
2022-08-09 19:13:48 +0300

    Done homework 12

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-
-        int i = 1;
-        System.out.println(++i);
+        System.out.println("Homework done!");
     }
 }
